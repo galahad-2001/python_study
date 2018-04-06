28f9099025c125fb0223f00166b3ef5402a74860
Jayden Gu <galahad-2001@163.com>
2018-04-06 13:38:21 +0800

    update

--- spider_study.py
+++ spider_study.py
@@ -9,10 +9,12 @@ hds = [{'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.
        {'User-Agent': 'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11'},
        {'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)'}]
 
+
 def get_source_code(url):
     req = urllib2.Request(url)
     source_code = urllib2.urlopen(req).read()
     return source_code
+
 
 def get_subject_items(source_code):
     soup = BeautifulSoup(source_code, "html.parser")
@@ -20,23 +22,23 @@ def get_subject_items(source_code):
     subject_items = subject_list.findAll('li', {'class': 'subject-item'})
     return subject_items
 
-def print_subject_items(subject_items):
+
+def get_list_subject_items(subject_items):
+    list_subject_items = []
     for subject_item in subject_items:
         title = subject_item.find('div', {'class': 'info'}).a.get_text().strip().replace(' ', '').replace('\n', '')
         pub = subject_item.find('div', {'class': 'pub'}).get_text().strip()
         rating_nums = subject_item.find('span', {'class': 'rating_nums'}).get_text().strip()
-        pl = subject_item.find('span', {'class': 'pl'}).get_text().strip()
+        pl = subject_item.find('span', {'class': 'pl'}).get_text().strip().strip('(').strip(u'人评价)')
         p = subject_item.p.get_text().strip().replace('\n', '')
-        # print title
-        # print pub
-        # print rating_nums
-        # print pl
-        # print p
-        # print '*' * 150
+        list_subject_items.append([title, pub, rating_nums, rating_nums, p])
+    return list_subject_items
 
-i = 0
-while i <= 41:
-    url = 'https://book.douban.com/tag/%E5%B0%8F%E8%AF%B4?start=' + str(i) + '&type=T'
-    # print url
-    print_subject_items(get_subject_items(get_source_code(url)))
-    i += 20
+
+if __name__ == '__main__':
+    i = 0
+    while i <= 1:
+        url = 'https://book.douban.com/tag/%E5%B0%8F%E8%AF%B4?start=' + str(i) + '&type=T'
+        # print url
+        print get_list_subject_items(get_subject_items(get_source_code(url)))
+        i += 20
